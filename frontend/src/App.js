5c441c1d929e7ca51f06db8c4e4f720a0d6b2bbd
HanCatNguyen <nguyencat.han@gmail.com>
2023-02-12 14:18:47 -0500

    Flash words

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import ShowWord from "./components/ShowWord";
+import text from "./data/single_words_easy.txt"
 
 function App(){
     const [appState, setStart] = useState(false);
@@ -7,11 +8,19 @@ function App(){
     const handleClick = () => {
         setStart(true);
     };
+
+    // fetch(text)
+    // .then(r => r.text())
+    // .then(text => {
+    //     console.log('text decoded:', text);
+    // });
+
     return(
         <div>
             <button onClick={handleClick}>Start</button>
             {
-                appState && <ShowWord word="Hello"/> 
+                // appState && <ShowWord word="Hello"/> 
+                appState && <ShowWord file={text} delay={1000}/> 
             }
                        
         </div>
--- frontend/src/components/ShowWord.js
+++ frontend/src/components/ShowWord.js
@@ -1,7 +1,44 @@
-// This is the code to show words on the screen
-function ShowWord({word}){
-    return(<p>{word}</p>
-    );
+import React, { useState, useEffect, useRef } from 'react';
+
+function ShowWord({file, delay}){
+    // Initialize wordArray to store words that will flash on screen
+    var wordArray = []
+
+    // Read from the file and populate the wordArray
+    fetch(file)
+    .then(r => r.text())
+    .then(text => {
+        wordArray = text.split("\n");
+    });
+
+
+    // Set the state to allow for flashing words
+    const [currWord, setCurrWord] = useState(wordArray[0]);
+	const [isActive, setIsActive] = useState(true);
+
+	const index = useRef(0);
+
+    // Flashing word effect
+	useEffect(() => {
+		let interval = null;
+		if (isActive) {
+			interval = setInterval(() => {
+				index.current++;
+				setCurrWord(wordArray[index.current]);
+				if (index.current === wordArray.length - 1) {
+					setIsActive(false);
+				}
+			}, delay); // Sets the time interval in milliseconds
+		}
+		return () => clearInterval(interval);
+	});
+
+    // Returns the word in a styled fashion
+	return (
+		<div style={{textAlign: "center"}}>
+			<h1>{currWord}</h1>
+		</div>
+	);
 }
 
 export default ShowWord;
