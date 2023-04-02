d8eeae811d9e412631a8fb33616846297056d5c5
Kusha Sareen <kushasareen42@gmail.com>
2023-04-02 08:07:48 -0400

    CNN + parsing

--- siggy_ml/parse_offline_data.py
+++ siggy_ml/parse_offline_data.py
@@ -1,11 +1,27 @@
 import numpy as np 
 import pandas as pd
+import matplotlib.pyplot as plt
+from erp_classifier import stupid_clf 
 
-wordlist_filename = "test_data.txt"
-data_filename = "Fin_List_sent.xlsx"
-outout_filename = ""
+wordlist_filename = "Fin_List_sent.csv"
+data_filename = "test_data.txt"
+output_filename = ""
 
-wordlist_df = pd.read_excel(wordlist_filename)
+wordlist_df = pd.read_csv(wordlist_filename)
+wordlist_df.columns = ['sentences', 'words', 'prob']
 data = pd.read_csv(data_filename, sep = "	")
 
-print(wordlist_df)
+def check_word(w):
+	return w in wordlist_df['words'].to_numpy()
+
+print(check_word('banana'), check_word('clook'))
+
+channels = []
+
+sample = data.iloc[:, 3].to_numpy()
+plt.plot(sample)
+clf = stupid_clf()
+processed = clf.preprocess(np.array([sample]))
+print(processed.shape)
+plt.plot(processed[0])
+plt.show()
